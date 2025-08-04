d90c857fc2923fbff20fa020f12c6c85c041094f
rahuly99390587-lab <rahuly99390587@gmail.com>
2025-08-04 15:08:05 +0530

    Task 2: Create a Personalized Greeting

---
+++ ass 1 q2.py
@@ -0,0 +1,9 @@
+Python 3.13.5 (v3.13.5:6cb20a219a8, Jun 11 2025, 12:23:45) [Clang 16.0.0 (clang-1600.0.26.6)] on darwin
+Enter "help" below or click "Help" above for more information.
+>>> first=input("enter your first name")
+enter your first name rahul 
+>>> last=input("enter your last name ")
+enter your last name yadav
+>>> full_name=first+""+last
+>>> print("hello,",full_name,"welcome to python programming")
+hello,  rahul yadav welcome to python programming
