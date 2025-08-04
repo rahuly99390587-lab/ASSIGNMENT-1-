578a3d6ee5de00cc925f6aa3c646f5ea76a97958
rahuly99390587-lab <rahuly99390587@gmail.com>
2025-08-04 15:07:15 +0530

    Task 1: Perform Basic Mathematical Operations

---
+++ main.py
@@ -0,0 +1,16 @@
+#taking input from user 
+no1=float(input("enter your first number"))
+no2=float(input("enter your second number"))
+
+#performing basic operations
+
+addition=no1+no2
+subtraction=no1-no2
+multiplication=no1*no2
+division=no1/no2
+
+# displaying the results 
+print(addition)
+print(subtraction)
+print(multiplication)
+print(division)
